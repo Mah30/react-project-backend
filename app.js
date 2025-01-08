1f7e0a95644f2e4a76197fc81ccb693803748f73
Mariah <alinemariah@hotmail.com>
2025-01-08 11:04:09 +0100

    add images and modified app db package and packagejs

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const server = jsonServer.create();
 
 const router = jsonServer.router('db.json');
 
+const express = require("express") 
+
+const sharp = require('sharp');
+
 const middlewares = jsonServer.defaults();
 const PORT = 4000;
 
+server.use(express.static ("publico"))
 server.use(middlewares);
 server.use(morgan('dev'));
 server.use((req, res, next) => {
@@ -16,6 +21,28 @@ server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
+
+server.get('/generate-image', async (req, res) => {
+  try {
+    const semiTransparentRedPng = await sharp({
+      create: {
+        width: 48,
+        height: 48,
+        channels: 4,
+        background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+      },
+    })
+      .jpg()
+      .toBuffer();
+
+    // Envia a imagem como resposta
+    res.type('image/png').send(semiTransparentRedPng);
+  } catch (err) {
+    console.error('Erro ao gerar a imagem:', err);
+    res.status(500).send('Erro ao gerar a imagem');
+  }
+});
+
 server.use(router);
 
 server.listen(PORT, () => {
